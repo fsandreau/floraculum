2e132c9f0856cf0c5d85f4b05443312e1b642474
Franck Nganiet <franck@nganiet.fr>
2024-08-18 18:32:29 -0400

    Update Workflow

--- build/server/next-font-manifest.js
+++ build/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Volumes/DATA/Code/plant-identifier-claude/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\",\"static/media/3a8dc5763a8ae4b2-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
